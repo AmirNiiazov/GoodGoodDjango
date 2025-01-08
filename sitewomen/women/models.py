f313c3fed06cd4a178b26f6b6df7479f4aac501a
AmirNiiazov <amirjonniiazov777@gmail.com>
2025-01-08 22:33:15 +0500

    3.3 CRUD

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Women(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(null=True, blank=True)
-    is_published = models.BooleanField(default=False)
+    is_published = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
